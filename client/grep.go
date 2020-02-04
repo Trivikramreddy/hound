d5b1c85f4bda0bb486db9689544dee5ac4f9dcba
Trivikramreddy <trivikramchanti@gmail.com>
2020-02-04 10:35:08 -0700

    Update grep.go

--- client/grep.go
+++ client/grep.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"regexp"
 
-	"github.com/it-projects-llc/hound/ansi"
-	"github.com/it-projects-llc/hound/config"
+	"github.com/Trivikramreddy/hound/ansi"
+	"github.com/Trivikramreddy/hound/config"
 )
 
 type grepPresenter struct {
