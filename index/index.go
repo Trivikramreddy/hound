f58fecc9f90c60e959deff5fff63a137244c6518
Trivikramreddy <trivikramchanti@gmail.com>
2020-02-04 10:43:34 -0700

    Update index.go

--- index/index.go
+++ index/index.go
@@ -12,8 +12,8 @@ import (
 	"time"
 	"unicode/utf8"
 
-	"github.com/it-projects-llc/hound/codesearch/index"
-	"github.com/it-projects-llc/hound/codesearch/regexp"
+	"github.com/Trivikramreddy/hound/codesearch/index"
+	"github.com/Trivikramreddy/hound/codesearch/regexp"
 )
 
 const (
